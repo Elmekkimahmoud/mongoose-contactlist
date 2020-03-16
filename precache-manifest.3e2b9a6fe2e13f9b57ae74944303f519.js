c27ab93623bd1e70c6a6da4b7b9ffa873b8e1375
Elmekkimahmoud <59558915+Elmekkimahmoud@users.noreply.github.com>
2020-03-16 13:39:22 +0100

    Updates

---
+++ precache-manifest.3e2b9a6fe2e13f9b57ae74944303f519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "392a77df4d6c0dafe67183a59912e77d",
+    "url": "/mongoose-contactlist/index.html"
+  },
+  {
+    "revision": "10710fec0555d27b23fb",
+    "url": "/mongoose-contactlist/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "93ff654779987c12bc6c",
+    "url": "/mongoose-contactlist/static/js/2.d93d1f07.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/mongoose-contactlist/static/js/2.d93d1f07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10710fec0555d27b23fb",
+    "url": "/mongoose-contactlist/static/js/main.9ace7ec7.chunk.js"
+  },
+  {
+    "revision": "e32e25df5f697822012b",
+    "url": "/mongoose-contactlist/static/js/runtime-main.6dcd7e2c.js"
+  }
+]);
